f97bf4bbc4cee48c450b7b6166894ee9899c7eda
Dharaneesh <dharaneeshperumal27@gmail.com>
2024-03-03 17:11:18 +0530

    Create StudentGradeCalculator.java

---
+++ StudentGradeCalculator.java
@@ -0,0 +1,54 @@
+package CodSoft;
+
+import java.util.Scanner;
+
+public class StudentGradeCalculator {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Input: Take marks obtained in each subject
+        System.out.println("Enter marks obtained (out of 100) in each subject:");
+        int numSubjects = 5; // You can change this based on the actual number of subjects
+        int[] marks = new int[numSubjects];
+
+        for (int i = 0; i < numSubjects; i++) {
+            System.out.print("Subject " + (i + 1) + ": ");
+            marks[i] = scanner.nextInt();
+        }
+
+        // Calculate Total Marks
+        int totalMarks = 0;
+        for (int mark : marks) {
+            totalMarks += mark;
+        }
+
+        // Calculate Average Percentage
+        double averagePercentage = (double) totalMarks / numSubjects;
+
+        // Grade Calculation
+        char grade = calculateGrade(averagePercentage);
+
+        // Display Results
+        System.out.println("\nResults:");
+        System.out.println("Total Marks: " + totalMarks);
+        System.out.println("Average Percentage: " + averagePercentage + "%");
+        System.out.println("Grade: " + grade);
+
+        scanner.close();
+    }
+
+    private static char calculateGrade(double averagePercentage) {
+        if (averagePercentage >= 90) {
+            return 'A';
+        } else if (averagePercentage >= 80) {
+            return 'B';
+        } else if (averagePercentage >= 70) {
+            return 'C';
+        } else if (averagePercentage >= 60) {
+            return 'D';
+        } else {
+            return 'F';
+        }
+    }
+}
