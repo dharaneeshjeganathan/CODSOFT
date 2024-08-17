016575250036126664c08c67ae1611385f28aacb
Dharaneesh <dharaneeshperumal27@gmail.com>
2024-08-17 23:07:47 +0530

    Fix : Changes in NumberGame.java

--- NumberGame.java
+++ NumberGame.java
@@ -1,20 +1,16 @@
 package CodSoft;
-
 import java.util.Random;
 import java.util.Scanner;
 
 public class NumGame {
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Random random = new Random();
-        
+        Random random = new Random(); 
         int lowerBound = 1;
         int upperBound = 100;
         int attemptsLimit = 10;
         int rounds = 0;
         int totalAttempts = 0;
-        
         System.out.println("Welcome to the Number Game!");
         
         do {
@@ -23,7 +19,7 @@ public class NumGame {
 
             int attempts = 0;
             boolean correctGuess = false;
-
+            
             while (attempts < attemptsLimit) {
                 System.out.print("Enter your guess: ");
                 int userGuess = scanner.nextInt();
@@ -39,7 +35,6 @@ public class NumGame {
                     System.out.println("Too high. Try again.");
                 }
             }
-
             if (!correctGuess) {
                 System.out.println("Sorry, you've reached the maximum number of attempts. The correct number was: " + targetNumber);
             } else {
@@ -47,10 +42,8 @@ public class NumGame {
                 rounds++;
                 System.out.println("Your score for this round: " + attempts + " attempts");
             }
-
             System.out.print("Do you want to play again? (yes/no): ");
         } while (scanner.next().equalsIgnoreCase("yes"));
-
         System.out.println("Game Over! You played " + rounds + " rounds and your average score was " + (totalAttempts / rounds) + " attempts per round.");
         scanner.close();
     }
